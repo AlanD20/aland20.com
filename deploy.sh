60617cda3616af85b84dc375fbdfcf43b23ea89c
AlanD20 <aland20@pm.me>
2024-10-12 12:45:41 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-USER=
-SCRIPT_PATH=
-BASE_PATH=
+user=$1
+backup_path=$2
+project_path=$3
+
+if [[ $user == "" || $backup_path == "" || $project_path == "" ]]; then
+  echo "- [x] Args are required: (1) user (2) absolute script path (3) absolute project path"
+  exit 1
+fi
 
 # Find bin directory path for global npm packages using 'npm config get prefix'
 PATH=$PATH:/home/$USER/.nvm/versions/node/lts/bin
@@ -10,64 +15,54 @@ PATH=$PATH:/home/$USER/.nvm/versions/node/lts/bin
 # Stop portfolio service
 pm2 delete nextjs-portfolio && pm2 save -f
 
-# Copy database file if exists
-if [ -f "$BASE_PATH/prisma/db.sqlite" ]; then
-  cp -f "$BASE_PATH/prisma/db.sqlite" "$SCRIPT_PATH/db.sqlite"
+if $?; then
+  echo "- [x] unable to delete existing project from pm2 list"
+  exit 1
 fi
 
-# Copy .env file if exists
-if [ -f "$BASE_PATH/.env" ]; then
-  cp -f "$BASE_PATH/.env" "$SCRIPT_PATH/.env"
-fi
+files_need_backup=(
+  "prisma/db.sqlite"  # current database
+  ".env"              # .env file
+  "src/config/app.ts" # app configuration
+)
 
-# Copy config app file if exists
-if [ -f "$BASE_PATH/src/config/app.ts" ]; then
-  cp -f "$BASE_PATH/src/config/app.ts" "$SCRIPT_PATH/app.ts"
-fi
+# Take backups
+for file in "${files_need_backup[@]}"; do
+  if [ -f "$project_path/$file" ]; then
+    filename=$(basename "$file")
+    cp -f "$project_path/$file" "$backup_path/$filename"
+  fi
+done
 
 # Remove Everything in base path
-rm -rf $BASE_PATH/* $BASE_PATH/.*
+rm -rf "$project_path"
 
 # Clone Repository
-git clone https://github.com/AlanD20/aland20.com.git "$BASE_PATH/."
+git clone ssh://git@github.com:AlanD20/aland20.com.git "$project_path/."
 
 # Recover .env file if exists
-if [ -f "$SCRIPT_PATH/.env" ]; then
-  cp -f "$SCRIPT_PATH/.env" "$BASE_PATH/.env"
+if [ -f "$backup_path/.env" ]; then
+  cp -f "$backup_path/.env" "$project_path/.env"
 fi
+
+# Recover Backups
+for file in "${files_need_backup[@]}"; do
+  filename=$(basename "$file")
+  if [ -f "$backup_path/$filename" ]; then
+    cp -f "$backup_path/$filename" "$project_path/$file"
+    echo "- [x] Successful restore: $file"
+    continue
+  fi
+
+  echo "- [x] Failed to restore: $file"
+done
 
 # Install dependencies
-cd "$BASE_PATH" && yarn && yarn db:reset
+cd "$project_path" && pnpm && pnpm db:push && pnpm build && pnpm pm2 && pm2 save -f
 
-# Recover Config file if exists
-if [ -f "$SCRIPT_PATH/app.ts" ]; then
-  cp -f "$SCRIPT_PATH/app.ts" "$BASE_PATH/src/config/app.ts"
+if $?; then
+  echo "- [x] Failed to build project"
+  exit 1
 fi
 
-# Recover database if exists
-if [ -f "$SCRIPT_PATH/db.sqlite" ]; then
-  cp -f "$SCRIPT_PATH/db.sqlite" "$BASE_PATH/prisma/db.sqlite"
-fi
-
-# Check Setup Process
-if [ -f "$BASE_PATH/.env" ]; then
-  echo "- [x] Env file copied!"
-else
-  echo "- [x] Failed to copy env file!"
-fi
-
-if [ -f "$BASE_PATH/prisma/db.sqlite" ]; then
-  echo "- [x] Database restored!"
-else
-  echo "- [x] Failed to restore database!"
-fi
-
-if grep -Fq "https://aland20.com" "$BASE_PATH/src/config/app.ts"; then
-  echo "- [x] Config file updated!"
-else
-  echo "- [x] Failed to update config file!"
-fi
-
-cd "$BASE_PATH" && yarn build && yarn pm2 && pm2 save -f
-
-echo "- [x] Deployment Successsful!"
+echo "- [x] Deployment successful!"
